f2ff5a4671221741fdfcabd542118106e32b919c
Mileno Valdo <milex2222@gmail.com>
2019-10-24 15:41:16 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')   
+
+client.load_extension("bot")                #Loads bot.py
+
+with open('txt/token.txt', 'r') as fp:
+    token = fp.readline()
+client.run(token)                           #Runs the bot with the loaded extensions
---
+++ test.py
@@ -0,0 +1,4 @@
+def f(x):
+    return x + 2, x * 2
+x, y = f(5)
+print(x + y)
