85836218d235819db75d3a70e7953b75f2933b71
Mileno Valdo <milex2222@gmail.com>
2019-10-28 13:34:21 +0700

    Completed restructuring
    Restructured the entire project to have multiple classes so it is more organized

---
+++ cogs/fun.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import urllib.request, json
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    '''
+    This command sends an API request to steam
+    and respond with data about the specified steam account
+    '''
+    @commands.command(aliases = ['hourswasted', 'getdisappointed', 'steam', 'gamerflex'])
+    async def steamhours(self, ctx, steamid):
+        with open('txt/steamapi.txt', 'r') as fp:
+            apikey = fp.readline()
+
+        steamAPIURL = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={apikey}&steamid={steamid}&include_played_free_games=False'
+        steamAPIUsrURL = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={apikey}&steamids={steamid}'
+        totalMinutes = 0
+
+        with urllib.request.urlopen(steamAPIUsrURL) as userurl:
+            response = json.loads(userurl.read().decode())
+            steamUser = response['response']['players'][0]
+        with urllib.request.urlopen(steamAPIURL) as url:
+            data = json.loads(url.read().decode())
+            for game in data['response']['games']:
+                totalMinutes += game['playtime_forever']
+
+        embed = discord.Embed(title = 'Gamer Flex')
+        embed.set_thumbnail(url = steamUser['avatarfull'])
+        embed.add_field(name = steamUser["personaname"], value = f'Total hours played: **{round(totalMinutes/60)}**')
+        await ctx.channel.send(content=None, embed = embed)
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
---
+++ cogs/listener.py
@@ -0,0 +1,153 @@
+import discord
+from discord.ext import commands
+from random import randint
+from time import sleep
+import json
+
+class Listener(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def levelUpAnnouncement(self, mention, role):
+        return f'{mention} have leveled up to {role}'
+    
+    '''
+    Notifies us when the bot is ready to function.
+    '''
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Logged in as {self.bot.user}')  
+        await self.bot.change_presence(status = discord.Status.online, activity = discord.Game('Ready to accept commands'))   
+
+    '''
+    Creates an announcement when someone joins the server, sends
+    the new member the rules of the server in private message, and
+    adds their ID to the json.
+    '''
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        #Announcement
+        channel = self.bot.get_channel(635840445079093250)
+        await channel.send(f'Please welcome {member.mention} to the server')
+        #Rules messaging
+        await member.send(
+            f'Welcome to Binus Discord server \n' +
+            f'Please read the rules below: \n' +
+            f'1. Do not be annoying \n' +
+            f'2. Do not send inflammatory messages in the server or to anyone in the server \n' +
+            f'Breaking these rules will result in a perma ban.'
+        )
+        #Registering the new user to the json
+        with open('json/userxp.json') as fp:
+            users = json.load(fp)
+        if str(member.id) not in users["users"]:
+            users["users"].update({str(member.id): 0})
+            with open('json/userxp.json', 'w') as fileToDump:
+                json.dump(users, fileToDump, indent = 4)
+        else:
+            pass
+
+    '''
+    Creates an announcement when someone gets banned from the server
+    and removed them from userxp.json
+    '''
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, member):
+        with open('json/userxp.json') as fp:
+            users = json.load(fp)
+        try:
+            del users["users"][str(member.id)]
+            with open('json/userxp.json', 'w') as fileToDump:
+                json.dump(users, fileToDump, indent = 4)
+        except KeyError:
+            print('Key doesn\'t exist. Add member id manually to the json.')
+
+    '''
+    Creates an announcement when someone gets unbanned in the server
+    '''
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild, member):
+        channel = self.bot.get_channel(635840445079093250)
+        await channel.send(f'User {member.mention} has been unbanned from the server', delete_after = 10)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        '''
+        Bad words filtering
+        Checks if every word contained in the message is in the list of 
+        dirty words.
+        If yes, then the bot delete the last message the sends a reminder
+        as a response.
+        '''
+        if message.author.id == 635833390238793729:
+            return
+
+        mentionUser = message.author.mention
+        responseList = [
+            f'{mentionUser} Please keep it clean bro',
+            f'{mentionUser} Did your mom not raise you properly bro?',
+            f'{mentionUser} I\'m calling the police on you bro'
+        ]
+        
+        with open('txt/dirtywords.txt', 'r') as fp:                 
+            dirtywords = fp.read().splitlines()
+        for word in message.content.split():
+            if word.lower() in dirtywords:
+                await message.delete()
+                await message.channel.send(responseList[randint(0, len(responseList)-1)], delete_after = 10)
+        '''
+        Checks if user who wrote a message is in the "people to harass"
+        list. If yes, the bot will respond with a harassment message
+        '''
+        with open('txt/peopletoharass.txt', 'r') as fp:
+            harassUserList = fp.read().splitlines()
+        with open('txt/harassmentreponses.txt', 'r') as fp:
+            harassmentResponses = fp.read().splitlines()
+        if str(message.author) in harassUserList:
+            try:
+                randNum = randint(0, len(harassmentResponses) - 1)
+                await message.channel.send(f'{message.author.mention} {harassmentResponses[randNum]}')
+                if randNum == 6:
+                    sleep(5)
+                    await message.channel.send(f'{message.author.mention} Sike, b*tch')
+                else:
+                    pass
+            except IndexError:
+                print('Harassment response empty. Fill in the "harassmentreponses.txt"')
+        '''
+        Level up system
+        adds experience points every time a user sends a message
+        in the server
+        '''
+        with open('json/userxp.json') as fp:
+            users = json.load(fp)
+        users['users'][str(message.author.id)] += 1
+        with open('json/userxp.json', 'w') as fileToDump:
+            json.dump(users, fileToDump, indent = 4)
+        authorXP = users['users'][str(message.author.id)]
+        if authorXP == 10:
+            role = discord.utils.get(message.guild.roles, id = 638181731794681866)
+            await message.author.add_roles(role)
+            await message.channel.send(self.levelUpAnnouncement(message.author.mention, role.name))
+        elif authorXP == 30:
+            role = discord.utils.get(message.guild.roles, id = 638181995909873694)
+            await message.author.add_roles(role)
+            await message.channel.send(self.levelUpAnnouncement(message.author.mention, role.name))
+        elif authorXP == 90:
+            role = discord.utils.get(message.guild.roles, id = 638182182136840202)
+            await message.author.add_roles(role)
+            await message.channel.send(self.levelUpAnnouncement(message.author.mention, role.name))
+        elif authorXP == 270:
+            role = discord.utils.get(message.guild.roles, id = 638182260264403024)
+            await message.author.add_roles(role)
+            await message.channel.send(self.levelUpAnnouncement(message.author.mention, role.name))
+        elif authorXP == 810:
+            role = discord.utils.get(message.guild.roles, id = 638182302408769571)
+            await message.author.add_roles(role)
+            await message.channel.send(self.levelUpAnnouncement(message.author.mention, role.name))
+
+'''
+Sets up the bot object as a cog
+'''
+def setup(bot):
+    bot.add_cog(Listener(bot))
