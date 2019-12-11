0d9d657f361f7b337c5bc7012bc1e6e586202aa2
Mileno Valdo <milex2222@gmail.com>
2019-12-11 17:05:04 +0700

    Fixed datatype bug
    Fixed txt file read converting integers into strings in the list.

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,6 +6,8 @@ class Admin(commands.Cog):
         self.bot = bot
         with open('txt/whitelist.txt', 'r') as fp:
             self.whitelist = fp.read().splitlines()
+        self.whitelist = list(map(int, self.whitelist))
+        print(self.whitelist)
         
 
     def noPerm(self):
@@ -23,7 +25,7 @@ class Admin(commands.Cog):
     '''
     @commands.command(aliases = ['clear', 'clr'])
     async def clearMessages(self, ctx, numberOfMessages: int = 10):
-        if str(ctx.author.id) in self.whitelist:
+        if ctx.author.id in self.whitelist:
             await ctx.channel.purge(limit = (numberOfMessages + 1))
         else:
             await ctx.channel.send(self.noPerm())
@@ -35,7 +37,7 @@ class Admin(commands.Cog):
     async def announce(self, ctx, *, message):
         sendChannel = self.bot.get_channel(635840445079093250)                                  #Specifies which channel to send the response to
 
-        if str(ctx.author.id) in self.whitelist and ctx.channel.id == 636045039386230784:        #Checks to see if the command is written by a whitelisted user and in the right channel
+        if ctx.author.id in self.whitelist and ctx.channel.id == 636045039386230784:        #Checks to see if the command is written by a whitelisted user and in the right channel
             await sendChannel.send(f'@everyone {message}')
         else:
             await ctx.channel.send(self.noPerm())
@@ -47,7 +49,7 @@ class Admin(commands.Cog):
     async def kick(self, ctx, member: discord.Member, *, reason = None):
         sendChannel = self.bot.get_channel(635840445079093250)
 
-        if str(ctx.author.id) in self.whitelist: 
+        if ctx.author.id in self.whitelist: 
             await member.kick(reason = reason)
             await sendChannel.send(f'User {member.mention} has been kicked due to: {reason}', delete_after = 10)
         else:
@@ -60,7 +62,7 @@ class Admin(commands.Cog):
     async def ban(self, ctx, member: discord.Member, *, reason = None):
         sendChannel = self.bot.get_channel(635840445079093250)
 
-        if str(ctx.author.id) in self.whitelist: 
+        if ctx.author.id in self.whitelist: 
             await member.ban(reason = reason)
             await sendChannel.send(f'User {member.mention} has been banned due to: {reason}', delete_after = 10)
         else:
@@ -73,7 +75,7 @@ class Admin(commands.Cog):
     '''
     @commands.command()
     async def harass(self, ctx):
-        if str(ctx.author.id) in self.whitelist:
+        if ctx.author.id in self.whitelist:
             toBeAdded = ctx.message.mentions                #List of users mentioned in the message
 
             with open('txt/peopletoharass.txt', 'a') as fp:
@@ -84,9 +86,7 @@ class Admin(commands.Cog):
 
     @commands.command(name = 'addadmin', aliases = ['whitelist'])
     async def addAdmin(self, ctx, member: discord.Member):
-        print(str(ctx.author.id) in self.whitelist)
-
-        if str(ctx.author.id) in self.whitelist:
+        if ctx.author.id in self.whitelist:
 
             self.whitelist.append(member.id)
             with open('txt/whitelist.txt', 'w') as fp:
@@ -99,9 +99,9 @@ class Admin(commands.Cog):
 
     @commands.command(name = 'removeadmin', aliases = ['unwhitelist'])
     async def removeAdmin(self, ctx, member: discord.Member):
-        if str(ctx.author.id) in self.whitelist:
+        if ctx.author.id in self.whitelist:
 
-            self.whitelist.remove(str(member.id))
+            self.whitelist.remove(member.id)
             
             with open ('txt/whitelist.txt' , 'w') as fp:
                 for user in self.whitelist:
